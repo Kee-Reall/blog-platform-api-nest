c1d2716c91e1d26723795530b755ca7475f3059a
Kee-Reall <kirill_bezrodny@mail.ru>
2023-03-06 22:00:15 +0500

    every Query now work over Pipes

---
+++ src/posts/posts.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { PostsQueryPipe } from './pipes/posts.query.pipe';
+
+describe('PostsPipe', () => {
+  it('should be defined', () => {
+    expect(new PostsQueryPipe()).toBeDefined();
+  });
+});
