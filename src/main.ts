a79507c98d5eedfed4c85f87c164fd2d8d26f4a6
Kee-Reall <kirill_bezrodny@mail.ru>
2023-03-06 22:51:37 +0500

    class validator and class transformer added to the project dependency

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
     credentials: true,
     origin: process.env.FRONTEND_DOMAIN ?? 'http//localhost:3000/',
   });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port, () => console.log('Application port: ' + port));
 }
 
