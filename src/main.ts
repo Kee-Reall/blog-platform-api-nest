69a31df43703a598334092263907625ebfaf9286
Kee-Reall <kirill_bezrodny@mail.ru>
2023-03-16 01:02:58 +0500

    cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { GlobalHTTPFilter } from './app.filter';
 import { exceptionFactory } from './helpers';
