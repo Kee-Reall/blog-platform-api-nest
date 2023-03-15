739afd14d96d67366adbc5bc08c3f791c7cce1d9
Kee-Reall <kirill_bezrodny@mail.ru>
2023-03-16 02:55:03 +0500

    fox

--- src/helpers/enums/message.enum.ts
+++ src/helpers/enums/message.enum.ts
@@ -5,7 +5,7 @@ export enum MessageENUM {
   REQUIRED_FIELD = 'A field is required',
   NOT_EXIST = 'A field does not exist',
   NOT_ALLOW = 'A field is expired, not exist, or operation forbidden',
-  SYMBOLS = 'field has prohibited symbols',
+  SYMBOLS = 'A field has prohibited symbols',
   FORBIDDEN_VALUE = 'field has forbidden value',
   READONLY = 'attempt change readonly value',
   ALREADY_EXISTS = 'field already use',
