2bbc8e8e9e8a2f23afd24d07f2a093313d3adbd0
Kee-Reall <kirill_bezrodny@mail.ru>
2023-04-08 21:15:03 +0500

    made ban model

---
+++ src/Model/Schema/ban.schema.ts
@@ -0,0 +1,20 @@
+import { ObjectId } from 'mongodb';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type BanDocumet = HydratedDocument<Ban>;
+
+/*in ban list for blogger. Global ban contain in user*/
+
+@Schema()
+class Ban {
+  @Prop({ required: true, readonly: true, ref: 'User' }) ownerId: ObjectId;
+  @Prop({ required: true, readonly: true, ref: 'User' }) bannedUserId: ObjectId;
+  @Prop({ required: true, readonly: true }) bannedUserLogin: 'string';
+  @Prop({ required: true, readonly: true, ref: 'Blog' }) blogId: ObjectId;
+  @Prop({ default: true }) isBanned: boolean;
+  @Prop({ required: true }) banReason: string;
+  @Prop({ default: () => new Date() }) banDate: Date;
+}
+
+export const BanSchema = SchemaFactory.createForClass(Ban);
