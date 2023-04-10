7fe6a06713f9fdfd7891b73a0273fcb612e78c11
Kee-Reall <kirill_bezrodny@mail.ru>
2023-04-10 19:41:46 +0500

    add types

--- src/Public/controllers/blogs.controller.ts
+++ src/Public/controllers/blogs.controller.ts
@@ -1,18 +1,30 @@
 import { QueryBus } from '@nestjs/cqrs';
 import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { query } from '../useCases';
-import { BlogFilter, PostFilter, SoftGuardMeta } from '../../Model';
 import { Meta, SoftJwtGuard } from '../../Base';
+import {
+  BlogFilter,
+  BlogPresentationModel,
+  PaginatedOutput,
+  PostFilter,
+  PostPresentationModel,
+  SoftGuardMeta,
+  WithExtendedLike,
+} from '../../Model';
 
 @Controller('api/blogs')
 export class BlogsController {
   constructor(private bus: QueryBus) {}
   @Get()
-  public async getBlogs(@Query() filters: BlogFilter) {
+  public async getBlogs(
+    @Query() filters: BlogFilter,
+  ): Promise<PaginatedOutput<BlogPresentationModel>> {
     return await this.bus.execute(new query.GetBlogs(filters));
   }
   @Get(':id')
-  public async getBlogById(@Param('id') blogId: string) {
+  public async getBlogById(
+    @Param('id') blogId: string,
+  ): Promise<BlogPresentationModel> {
     return await this.bus.execute(new query.GetBlog(blogId));
   }
   @Get(':id/posts')
@@ -21,7 +33,7 @@ export class BlogsController {
     @Param('id') blogId: string,
     @Query() filters: PostFilter,
     @Meta() meta: SoftGuardMeta,
-  ) {
+  ): Promise<PaginatedOutput<WithExtendedLike<PostPresentationModel>>> {
     return await this.bus.execute(
       new query.GetPostsByBlog(meta.userId, blogId, filters),
     );
