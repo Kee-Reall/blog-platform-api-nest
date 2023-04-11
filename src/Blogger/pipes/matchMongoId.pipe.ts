7484201b11b5691c5f105c5dfdeb78d42f730f8b
Kee-Reall <kirill_bezrodny@mail.ru>
2023-04-11 22:18:30 +0500

    HT16 stable || checkfor 15 (add interface)

--- src/Blogger/pipes/matchMongoId.pipe.ts
+++ src/Blogger/pipes/matchMongoId.pipe.ts
@@ -1,7 +1,11 @@
-import { ArgumentMetadata, NotFoundException } from '@nestjs/common';
+import {
+  ArgumentMetadata,
+  NotFoundException,
+  PipeTransform,
+} from '@nestjs/common';
 import { isMongoId } from 'class-validator';
 
-export class MatchMongoIdPipe {
+export class MatchMongoIdPipe implements PipeTransform {
   public transform(value: string, metadata: ArgumentMetadata): string {
     if (!isMongoId(value)) {
       throw new NotFoundException();
